86f2c7fd594f3c95da9a50e8f087b47eb1f3ffcc
Jaiaid <jaiaidmobin@gmail.com>
2024-07-22 02:21:26 -0400

    he image generation and metric calculation

--- figures/metric_calc.py
+++ figures/metric_calc.py
@@ -32,6 +32,11 @@ def calc_contrast_michelson(frame):
 def calc_psnr(original_frame, modified_frame):
     return cv2.PSNR(original_frame, modified_frame)
 
+def mod_HE(original_frame):
+    dst = cv2.equalizeHist(cv2.cvtColor(original_frame, cv2.COLOR_BGR2GRAY))
+    dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    return dst
+
 if __name__ == '__main__':
     # we are assuming corresponding to original frame output frame is present
     for filename in os.listdir(os.path.join(FOLDER_ROOT, ORIGINAL_FOLDER)):
@@ -40,12 +45,19 @@ if __name__ == '__main__':
             continue
 
         original_frame = cv2.imread(os.path.join(os.path.join(FOLDER_ROOT, ORIGINAL_FOLDER), filename))
+        # HE equalized frame
+        he_frame = mod_HE(original_frame)
         original_contrast = calc_contrast_michelson(original_frame)
+        he_contrast = calc_contrast_michelson(he_frame)
+        he_psnr = calc_psnr(original_frame=original_frame, modified_frame=he_frame)
+        # save he image
+        cv2.imwrite(os.path.join(os.path.join(FOLDER_ROOT, "he"), basename + "_output.jpg"), he_frame, [cv2.IMWRITE_JPEG_QUALITY, 100])
 
-        data_list = [filename, original_contrast]
+        data_list = [filename, original_contrast, he_contrast, he_psnr]
         for method in METHODS_TO_COMPARE:
             # get the method corresponding out image
             out_frame = cv2.imread(os.path.join(os.path.join(FOLDER_ROOT, method), basename + "_output.jpg"))
+            
             # print(os.path.join(os.path.join(FOLDER_ROOT, method), basename + "_output.jpg"))
             psnr = calc_psnr(original_frame=original_frame, modified_frame=out_frame)
             contrast = calc_contrast_michelson(out_frame)
@@ -53,4 +65,4 @@ if __name__ == '__main__':
             data_list.append(psnr)
         # print(data_list)
         # print for latex tabular contrast & hc mod contrast & ga mod contrast & hc mod psnr & ga mod psnr
-        print(data_list[0], " & ".join(["{0:.3f}".format(data) for data in [data_list[1],data_list[2],data_list[4],data_list[3],data_list[5]]]))
+        print(data_list[0], " & ".join(["{0:.3f}".format(data) for data in [data_list[1], data_list[2],data_list[4],data_list[6], data_list[3],data_list[5],data_list[7]]]))
